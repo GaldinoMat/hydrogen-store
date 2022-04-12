10bee0acbcf4faaa17fa7d86f5cc2bd469b5d16a
Galdino <mateusgaldino1000@hotmail.com>
2022-04-11 22:57:35 -0300

    update: implement GraphCMS query usage

--- src/components/Home/Hero/HeroSliderData.js
+++
@@ -1,4 +0,0 @@
-export const backgroundImages = [
-  "url('https://cdn.shopify.com/s/files/1/0610/1870/1985/files/hero-1_jpg.webp?v=1649522164')",
-  "url('https://cdn.shopify.com/s/files/1/0610/1870/1985/files/hero-2_jpg.webp?v=1649551282')",
-];
