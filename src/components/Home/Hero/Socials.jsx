70b9221b83fe798d106acda094ac4a5701cfa980
Galdino <mateusgaldino1000@hotmail.com>
2022-04-15 13:27:34 -0300

    update: fix socials icons

--- src/components/Home/Hero/Socials.jsx
+++ src/components/Home/Hero/Socials.jsx
@@ -1,32 +1,32 @@
-// import {
-//   faFacebookF,
-//   faInstagram,
-//   faPinterest,
-//   faTwitter,
-// } from '@fortawesome/free-brands-svg-icons';
-// import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import Button from "../../Button.client";
+import {
+  faFacebookF,
+  faInstagram,
+  faPinterest,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import Button from '../../Button.client';
 
 export default function Socials() {
   return (
     <div className="mt-16 pb-8 sm:mx-4 md:mx-10 xl:mx-48 desktop:mx-96 flex">
       <Button
-        label={"hello"}
+        label={<FontAwesomeIcon className="w-4" icon={faFacebookF} />}
         url="/"
         className="px-0 w-5 h-6 rounded-none bg-transparent hover:bg-transparent"
       />
       <Button
-        label={"hello"}
+        label={<FontAwesomeIcon className="w-4" icon={faInstagram} />}
         url="/"
         className="w-5 h-6 rounded-none bg-transparent hover:bg-transparent"
       />
       <Button
-        label={"hello"}
+        label={<FontAwesomeIcon className="w-4" icon={faPinterest} />}
         url="/"
         className="w-5 h-6 rounded-none bg-transparent hover:bg-transparent"
       />
       <Button
-        label={"hello"}
+        label={<FontAwesomeIcon className="w-4" icon={faTwitter} />}
         url="/"
         className="w-5 h-6 rounded-none bg-transparent hover:bg-transparent"
       />
